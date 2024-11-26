1e50b8b454e5fe2f8a83e3b0e18a76ed0624fc92
Divine Orji <dpkreativ@gmail.com>
2024-11-26 22:56:09 +0100

    feature: setting up layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return <footer style={style}>I'm the footer</footer>;
+}
+
+const style = {
+  padding: "1rem",
+  backgroundColor: "var(--primary-color)",
+  color: "white",
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return <header style={style}>I'm the header</header>;
+}
+
+const style = {
+  padding: "1rem",
+};
