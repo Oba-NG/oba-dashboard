c25917094cc8fd359db362b7dbb1ef3642e8ade5
Divine Orji <dpkreativ@gmail.com>
2024-11-30 04:11:58 +0100

    feature: add reusable button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,41 @@
+/**
+ * Types of buttons we need:
+ * - Primary button: --oba-green background and border, white text
+ * - Secondary button: --bg-color background, black text and border
+ * - Tertiary button: --bg-color background, green text and border
+ * - Disabled button: --grey background and border, white text
+ *
+ * We need full width buttons and max-content width buttons
+ * Some buttons will have icons inside them.
+ *
+ * children: the text inside the button
+ * type: primary, secondary, tertiary, disabled
+ * width: full, max-content
+ * height: py-2, py-1
+ */
+
+const btn = {
+  primary: "bg-[--oba-green] text-white border border-[--oba-green]",
+  secondary: "bg-[--bg-color] text-black border border-black",
+  tertiary: "bg-[--bg-color] text-[--oba-green] border border-[--oba-green]",
+  disabled: "bg-[--grey] text-white border border-[--grey]",
+};
+
+export default function Button({ children, className, type }) {
+  return (
+    <button
+      className={`${
+        type === "disabled"
+          ? btn.disabled
+          : type === "secondary"
+          ? btn.secondary
+          : type === "tertiary"
+          ? btn.tertiary
+          : btn.primary
+      } 
+     rounded-md px-2 py-0.5 flex items-center justify-center gap-2 ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
+import { ShoppingCart } from "lucide-react";
+import Button from "../components/Button";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="p-5 max-w-[1200px] mx-auto">
+      {/* BUTTONS */}
+      <div className="flex gap-5 justify-center">
+        {/* Shopping cart button */}
+        <Button>
+          <ShoppingCart size={16} /> Add to cart
+        </Button>
+
+        {/* Pepper button */}
+        <Button>Pepper</Button>
+
+        {/* Meat & Poultry button */}
+        <Button type="secondary">Meat & Poultry</Button>
+
+        {/* Disabled button */}
+        <Button type="disabled">Disabled</Button>
+
+        {/* Random button */}
+        <Button>All</Button>
+      </div>
+    </main>
+  );
 }
