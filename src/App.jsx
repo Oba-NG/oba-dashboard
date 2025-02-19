62e55b70fbb7c405bb6a64ce9caecf372abdd164
Divine Orji <dpkreativ@gmail.com>
2025-02-19 05:15:24 +0100

    feature: favourite component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Vite from "./pages/Vite";
 
 export default function App() {
   return (
     <Layout>
-      <Home />
+      {/* <Home /> */}
+      <Vite />
     </Layout>
   );
 }
